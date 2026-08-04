4ecc4017042506c599fed23a7ff170b66ca8b7c5
chocomaltt <wijayarezaarya@gmail.com>
2026-08-04 16:09:05 +0700

    feat(cmd): changed env to viper on order service

--- apps/order-service/cmd/order-service/main.go
+++ apps/order-service/cmd/order-service/main.go
@@ -13,15 +13,18 @@ import (
 	grpcinterface "github.com/chocomaltt/ecommerce-go/apps/order-service/internal/interface/grpc"
 	identityusecase "github.com/chocomaltt/ecommerce-go/apps/order-service/internal/usecase/identity"
 	orderv1 "github.com/chocomaltt/ecommerce-go/common-rpc/gen/order/v1"
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	certificateFile := env("TLS_CERT_FILE", "../../deployments/compose/certs/order-service.crt")
-	keyFile := env("TLS_KEY_FILE", "../../deployments/compose/certs/order-service.key")
-	certificateAuthorityFile := env("TLS_CA_FILE", "../../deployments/compose/certs/ca.crt")
-	actorPublicKeyFile := env("ACTOR_PUBLIC_KEY_FILE", "../../deployments/compose/certs/identity-actor.pub")
-	port := env("GRPC_PORT", "9082")
+	initConfig()
+
+	certificateFile := viper.GetString("TLS_CERT_FILE")
+	keyFile := viper.GetString("TLS_KEY_FILE")
+	certificateAuthorityFile := viper.GetString("TLS_CA_FILE")
+	actorPublicKeyFile := viper.GetString("ACTOR_PUBLIC_KEY_FILE")
+	port := viper.GetString("GRPC_PORT")
 
 	credentials, err := grpcinterface.Credentials(certificateFile, keyFile, certificateAuthorityFile)
 	if err != nil {
@@ -30,7 +33,7 @@ func main() {
 
 	verifier, err := identity.NewVerifier(
 		actorPublicKeyFile,
-		env("ACTOR_ISSUER", "identity-service"),
+		viper.GetString("ACTOR_ISSUER"),
 		"order-service",
 	)
 	if err != nil {
@@ -60,6 +63,17 @@ func main() {
 	}
 }
 
+func initConfig() {
+	viper.SetDefault("TLS_CERT_FILE", "../../deployments/compose/certs/order-service.crt")
+	viper.SetDefault("TLS_KEY_FILE", "../../deployments/compose/certs/order-service.key")
+	viper.SetDefault("TLS_CA_FILE", "../../deployments/compose/certs/ca.crt")
+	viper.SetDefault("ACTOR_PUBLIC_KEY_FILE", "../../deployments/compose/certs/identity-actor.pub")
+	viper.SetDefault("ACTOR_ISSUER", "identity-service")
+	viper.SetDefault("GRPC_PORT", "9082")
+
+	viper.AutomaticEnv()
+}
+
 func shutdownOnSignal(ctx context.Context, server *grpc.Server) {
 	<-ctx.Done()
 
@@ -74,12 +88,4 @@ func shutdownOnSignal(ctx context.Context, server *grpc.Server) {
 	case <-time.After(5 * time.Second):
 		server.Stop()
 	}
-}
-
-func env(key, fallback string) string {
-	if value := os.Getenv(key); value != "" {
-		return value
-	}
-
-	return fallback
 }
